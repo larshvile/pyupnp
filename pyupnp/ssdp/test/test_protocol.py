f0ff382dba12e9921c8e2d724ccf7c7f18c42b4c
Lars Hvile <lars@hulte.net>
2013-02-17 20:03:26 +0100

    Converting from unittest to nose.

--- pyupnp/ssdp/test/test_protocol.py
+++ pyupnp/ssdp/test/test_protocol.py
@@ -1,61 +1,60 @@
-import unittest
-
+from nose.tools import *
 from pyupnp.ssdp.protocol import *
 
 MSEARCH = 'M-SEARCH * HTTP/1.1'
 _200OK = 'HTTP/1.1 200 OK'
 NOTIFY = 'NOTIFY * HTTP/1.1'
 
-class ProtocolTest(unittest.TestCase):
+class TestProtocol:
 
     def test_MSEARCH_message_yields_search_request(self):
         msg = parse_ssdp_message(msg_string(MSEARCH))
-        self.assertIsInstance(msg, SearchRequest)
+        assert type(msg) == SearchRequest
 
     def test_200OK_message_yields_search_reponse(self):
         msg = parse_ssdp_message(msg_string(_200OK))
-        self.assertIsInstance(msg, SearchResponse)
+        assert type(msg) == SearchResponse
 
     def test_NOTIFY_message_yields_advertisement(self):
         msg = parse_ssdp_message(msg_string(NOTIFY))
-        self.assertIsInstance(msg, Advertisement)
+        assert type(msg) == Advertisement
 
+    @raises(ParsingError)
     def test_other_messages_cannot_be_read(self):
-        with self.assertRaises(ParsingError):
-            parse_ssdp_message(msg_string('something else'))
+        parse_ssdp_message(msg_string('something else'))
 
     def test_headers_are_persisted_in_parsed_message(self):
         msg = parse_ssdp_message(msg_string(K1 = 'a', K2 = 'b'))
-        self.assertEqual('a', msg.headers['K1'])
-        self.assertEqual('b', msg.headers['K2'])
+        assert msg.headers['K1'] == 'a'
+        assert msg.headers['K2'] == 'b'
 
     def test_header_keys_are_stored_in_upper_case(self):
         msg = parse_ssdp_message(msg_string(key = 'v'))
-        self.assertIn('KEY', msg.headers)
+        assert 'KEY' in msg.headers
 
     def test_header_values_are_stripped_for_whitespace(self):
         msg = parse_ssdp_message(msg_string(key = ' v '))
-        self.assertEqual('v', msg.headers['KEY'])
+        assert msg.headers['KEY'] == 'v'
 
     def test_parsed_message_does_not_contain_empty_header(self):
         msg = parse_ssdp_message(msg_string())
-        self.assertNotIn('', msg.headers)
+        assert '' not in msg.headers
 
     def test_header_key_in_encoded_message_is_in_uppercase(self):
         msg = SearchRequest()
         msg.headers['test'] = 'some'
-        self.assertIn('TEST:', msg.encode())
+        assert 'TEST:' in msg.encode()
 
     def test_header_value_in_encoded_message_is_stripped_for_whitespace(self):
         msg = SearchRequest()
         msg.headers['test'] = ' value '
-        self.assertIn(': value\r', msg.encode())
+        assert ': value\r' in msg.encode()
 
     def test_headers_in_encoded_message_are_sorted_alphabetically(self):
         msg = SearchRequest()
         msg.headers['a'] = 'some'
         msg.headers['z'] = 'other'
-        self.assertIn('A: some\r\nZ: other', msg.encode())
+        assert 'A: some\r\nZ: other' in msg.encode()
 
     def test_encoded_message_is_identical_to_the_original(self):
         orig = 'M-SEARCH * HTTP/1.1\r\n'\
@@ -63,7 +62,7 @@ class ProtocolTest(unittest.TestCase):
                'MAN: "ssdp:discover\r\n'\
                'MX: 1\r\n'\
                'ST: ssdp:all\r\n'
-        self.assertEqual(orig, parse_ssdp_message(orig).encode())
+        assert parse_ssdp_message(orig).encode() == orig
 
 
 def msg_string(startline = MSEARCH, **headers):
